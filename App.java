0a575e7d526397c4232d060ed1a94038a6837e37
danielcollione <33942652+danielcollione@users.noreply.github.com>
2020-03-30 12:53:20 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+
+    private static Scanner sc = new Scanner(System.in);
+    private static List<String> listaAlunos = new ArrayList();
+
+    public static void main(String[] args) {
+
+    String option;
+
+        do {
+
+            System.out.println("\n----- Menu -----");
+            System.out.println("[1] - Inserir Aluno");
+            System.out.println("[2] - Imprimir Lista\n");
+            System.out.println("[0] - Sair\n");
+            option = sc.nextLine();
+
+            switch (option) {
+                case "1":
+                    inserirAluno();
+                    break;
+                case "2":
+                    exibirLista();
+                    break;
+                case "0":
+                    exibirLista();
+                    break;
+            }
+        } while (!option.equals("0"));
+    }
+
+    private static void inserirAluno() {
+        System.out.println("Insira o nome: ");
+        String nome = sc.nextLine();
+        if(verificaNomeExistente(nome)){
+            listaAlunos.add(nome);
+        }else{
+            System.out.println("\n#### NOME JA CADASTRADO ####\n");
+        }
+    }
+
+    private static boolean verificaNomeExistente(String nome){
+        listaAlunos.forEach(aluno ->{
+            if (aluno.equals(nome)){
+                return false;
+            }
+        });
+        return true;
+    }
+    verifica2 myverifica2 = new verifica2();
+    listaAlunos(rosterAsArray, varifica2::verificaNomeExistente);
+
+    private static void exibirLista() {
+        listaAlunos.forEach(System.out::println);
+    }
+}
